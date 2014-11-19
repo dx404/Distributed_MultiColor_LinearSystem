afa0ee9d831339435809de8733644ae5490f15ff
Duo "Donald" Zhao <wisdom.to.pilot@gmail.com>
2014-11-19 13:23:17 -0500

    v4

--- GS_OOP_v4.cpp
+++ GS_OOP_v4.cpp
@@ -1,5 +1,3 @@
-// halo.cpp
-
 // read regular
 // distribute
 // Z-order load
@@ -15,11 +13,8 @@
 #include <mpi.h>
 #include <map>
 #include <unordered_map>
-#include <tuple>
 
 using namespace std;
-
-
 
 #define send_data_tag 2001
 #define return_data_tag 2002
@@ -77,7 +72,9 @@ public:
 	int iExtBlockSize, jExtBlockSize, kExtBlockSize;
 	int blockBitWidth, blockSize;
 
-	vector<double> x_block, b_block;
+	vector<double> x_block, b_block; 
+
+	int halo_noneUpdate_width;
 
 private: 
 	static MPI::Status status;
@@ -86,7 +83,13 @@ private:
     int octMask_i, octMask_j, octMask_k;
     int i_bound_inter, j_bound_inter, k_bound_inter;
 
-	vector<vector<double> > sendHalo, recvHalo; // x
+    int i_update_low_zmix, i_update_high_zmix;
+    int j_update_low_zmix, j_update_high_zmix;
+    int k_update_low_zmix, k_update_high_zmix;
+
+	vector<vector<double> > sendHalo, recvHalo; 
+
+	double normChange;
 
 public: 
 	Capsule (int in_core_i, int in_core_j, int in_core_k) :
@@ -104,7 +107,6 @@ public:
 		jStart = core_j * jBlockSize - halo_width;
 		kStart = core_k * kBlockSize - halo_width;
 
-
 		iExtBlockSize = iBlockSize + 2*halo_width;
 		jExtBlockSize = jBlockSize + 2*halo_width; 
 		kExtBlockSize = kBlockSize + 2*halo_width;
@@ -115,18 +117,18 @@ public:
 	    octMask_i = 04444444444 & bitMask;
 	    octMask_j = 02222222222 & bitMask;
 	    octMask_k = 01111111111 & bitMask;
-	    
-	    i_bound_inter = memoInter[iExtBlockSize-1] << 2;
+
+		i_bound_inter = memoInter[iExtBlockSize-1] << 2;
 	    j_bound_inter = memoInter[jExtBlockSize-1] << 1;
 	    k_bound_inter = memoInter[kExtBlockSize-1];
-
-
+	    
 		x_block = vector<double> (blockSize, 0);
 		b_block = vector<double> (blockSize, 0);
 
 		sendHalo = haloInit ();
 		recvHalo = haloInit ();
 
+		halo_noneUpdate_width = halo_width ? 1 : 0;
 	}
 
 
@@ -147,23 +149,6 @@ public:
 			local_block[targetPos] = global_block[srcPos];
 		}
 		return local_block;
-	}
-
-	// inner 
-	vector<vector<double > > haloInit () {
-		vector<vector<double > > vacantHalo(27);
-		for (int di = -1; di <= 1; di++) 
-		for (int dj = -1; dj <= 1; dj++)
-		for (int dk = -1; dk <= 1; dk++) 
-			if (di | dj | dk) { 
-				int haloLinearID = (1+di)*9 + (1+dj)*3 + (1+dk);
-				int haloWidth_i = di ? halo_width : iBlockSize;
-				int haloWidth_j = dj ? halo_width : jBlockSize;
-				int haloWidth_k = dk ? halo_width : kBlockSize;
-				int haloSize = haloWidth_i * haloWidth_j * haloWidth_k;
-				vacantHalo[haloLinearID] = vector<double> (haloSize, 0);
-			}
-		return vacantHalo;
 	}
 
 	void prepareSendHalo () {
@@ -193,13 +178,12 @@ public:
 	double GS_Z_block_update () {
 		double normChange = 0;
 		for (int zID = 0; zID < x_block.size(); zID++) {
-			
-			// vector<int> regID = zDeMix(zID);
-			// int i_sub = regID[0], j_sub = regID[1], k_sub = regID[2];
+			vector<int> regID = zDeMix(zID);
+			int i_sub = regID[0], j_sub = regID[1], k_sub = regID[2];
 
-			int i_sub = memoInterR[(zID>>2) & octMask_k];
-			int j_sub = memoInterR[(zID>>1) & octMask_k];
-			int k_sub = memoInterR[ zID & octMask_k]; 
+			// int i_sub = memoInterR[(zID>>2) & octMask_k];
+			// int j_sub = memoInterR[(zID>>1) & octMask_k];
+			// int k_sub = memoInterR[ zID & octMask_k]; 
 
 			/*
 			if (i_sub >= iBlockSize + halo_width || j_sub >= jBlockSize + halo_width || k_sub >= kBlockSize + halo_width|| 
@@ -232,10 +216,8 @@ public:
 			x_block[zID] = val;
 		}
 		normChange = sqrt(normChange) / (iBlockSize * jBlockSize * kBlockSize);
-		return normChange;
+		return this->normChange = normChange;
 	}
-
-
 
 
 	double GS_Z_block_update002 () {
@@ -308,13 +290,25 @@ public:
 
 	double GS_Z_block_update003 () {
 		double normChange = 0;
+
+		i_bound_inter = memoInter[iExtBlockSize-1] << 2;
+	    j_bound_inter = memoInter[jExtBlockSize-1] << 1;
+	    k_bound_inter = memoInter[kExtBlockSize-1];
+
+	    i_update_low_zmix  = memoInter[halo_noneUpdate_width] << 2;
+	   	j_update_low_zmix  = memoInter[halo_noneUpdate_width] << 1;
+	   	k_update_low_zmix  = memoInter[halo_noneUpdate_width];
+
+	    i_update_high_zmix = memoInter[iExtBlockSize - halo_noneUpdate_width - 1] << 2;
+	    j_update_high_zmix = memoInter[jExtBlockSize - halo_noneUpdate_width - 1] << 1;
+	    k_update_high_zmix = memoInter[kExtBlockSize - halo_noneUpdate_width - 1];
+
 		int zAdjIDArray[27];
 		int i_comp[3], j_comp[3], k_comp[3];
 
 		for (int zID = 0; zID < x_block.size(); zID++) {
 			int iStep = 0, jStep = 0, kStep = 0;
 			// zNeighbors(zAdjIDArray, zID);
-
 			// int i_sub = memoInterR[(zID>>2) & octMask_k];
 			// int j_sub = memoInterR[(zID>>1) & octMask_k];
 			// int k_sub = memoInterR[ zID & octMask_k]; 
@@ -325,7 +319,7 @@ public:
 			// 	continue;
 			
 		    if (zID & octMask_i){
-		        i_comp[iStep++] = ((zID & octMask_i)  - 1) & octMask_i;
+		        i_comp[iStep++] = ((zID & octMask_i)  - 1)  & octMask_i;
 		    }
 		    i_comp[iStep++] = zID & octMask_i;
 		    if ((zID & octMask_i) != i_bound_inter){
@@ -365,15 +359,96 @@ public:
 			val = (b_block[zID] - val) / (-26.0);
 			normChange += (val - x_block[zID]) * (val - x_block[zID]);
 			x_block[zID] = val;
+			// cout << "::" << val << endl;
 
 		}
 
-		normChange = sqrt(normChange) / (iBlockSize * jBlockSize * kBlockSize);
+		normChange = sqrt(normChange) / (iExtBlockSize * jExtBlockSize * kExtBlockSize);
 		return normChange;
 	}
 
-	int haloSend () {
-		int ierr;
+	double GS_Z_block_update004 () {
+		double normChange = 0;
+
+	    i_update_low_zmix  = memoInter[halo_noneUpdate_width] << 2;
+	   	j_update_low_zmix  = memoInter[halo_noneUpdate_width] << 1;
+	   	k_update_low_zmix  = memoInter[halo_noneUpdate_width];
+
+	    i_update_high_zmix = memoInter[iExtBlockSize - halo_noneUpdate_width] << 2;
+	    j_update_high_zmix = memoInter[jExtBlockSize - halo_noneUpdate_width] << 1;
+	    k_update_high_zmix = memoInter[kExtBlockSize - halo_noneUpdate_width];
+
+		int zAdjIDArray[27];
+		int i_comp[3], j_comp[3], k_comp[3];
+
+		for (int zID = 0; zID < x_block.size(); zID++) {
+			int iStep = 0, jStep = 0, kStep = 0;
+			// zNeighbors(zAdjIDArray, zID);
+			// int i_sub = memoInterR[(zID>>2) & octMask_k];
+			// int j_sub = memoInterR[(zID>>1) & octMask_k];
+			// int k_sub = memoInterR[ zID & octMask_k]; 
+
+			if ((zID & octMask_i) < i_update_low_zmix || 
+				(zID & octMask_j) < j_update_low_zmix || 
+				(zID & octMask_k) < k_update_low_zmix || 
+				(zID & octMask_i) >= i_update_high_zmix || 
+				(zID & octMask_j) >= j_update_high_zmix || 
+				(zID & octMask_k) >= k_update_high_zmix ) {
+				continue;
+			}
+			
+		    if (zID & octMask_i){
+		        i_comp[iStep++] = ((zID & octMask_i)  - 1)  & octMask_i;
+		    }
+		    i_comp[iStep++] = zID & octMask_i;
+		    if ((zID & octMask_i) != i_bound_inter){
+		         i_comp[iStep++] = ((zID | (~octMask_i)) + 1) & octMask_i;
+		    }
+		    
+		    if (zID & octMask_j){
+		        j_comp[jStep++] = ((zID & octMask_j)  - 1) & octMask_j;
+		    }
+		    j_comp[jStep++] = zID & octMask_j;
+		    if ((zID & octMask_j) != j_bound_inter){
+		        j_comp[jStep++] = ((zID | (~octMask_j)) + 1) & octMask_j;
+		    }
+		    
+		    if (zID  & octMask_k){
+		        k_comp[kStep++] = ((zID & octMask_k)  - 1) & octMask_k;
+		    }
+		    k_comp[kStep++] = zID & octMask_k;
+		    if ((zID & octMask_k) != k_bound_inter){
+		       k_comp[kStep++] = ((zID | (~octMask_k)) + 1) & octMask_k;
+		    }
+
+		   int t = 0;
+		   for (int i = 0; i < iStep; i++)
+		   for (int j = 0; j < jStep; j++)
+		   for (int k = 0; k < kStep; k++) {
+		   		int nzID = i_comp[i] | j_comp[j] | k_comp[k];
+		    	if (nzID != zID)
+		        	zAdjIDArray[t++] = nzID;
+		    }
+
+			double val = 0;
+			for (int s = 0; s < t; s++) {
+					val += x_block[zAdjIDArray[s]];
+			}
+
+			val = (b_block[zID] - val) / (-26.0);
+			normChange += (val - x_block[zID]) * (val - x_block[zID]);
+			x_block[zID] = val;
+			// cout << "::" << val << endl;
+
+		}
+
+		normChange = sqrt(normChange) / (iExtBlockSize * jExtBlockSize * kExtBlockSize);
+		return this->normChange = normChange;
+	}
+
+	void haloSend () {
+		if (halo_width == 0) 
+			return;
 		for (int di = -1; di <= 1; di++) 
 		for (int dj = -1; dj <= 1; dj++)
 		for (int dk = -1; dk <= 1; dk++) 
@@ -383,22 +458,21 @@ public:
 			core_k + dk >= 0 && core_k + dk < numCore_k ) 
 		{
 			int destCoreLinear = (core_i + di) * numCore_j * numCore_k 
-								+ (core_j + dj) * numCore_k 
-								+ (core_k + dk);
+							   + (core_j + dj) * numCore_k 
+							   + (core_k + dk);
 
 			int srcHaloID  = (1+di)*9 + (1+dj)*3 + (1+dk);
 			// int destHaloID = (1-di)*9 + (1-dj)*3 + (1-dk); // implicit
 			int haloSize = sendHalo[srcHaloID].size();
-			if (haloSize) {
-				MPI::COMM_WORLD.Send (&sendHalo[srcHaloID][0], haloSize , MPI_DOUBLE, destCoreLinear, send_data_tag);
-			}
+			MPI::COMM_WORLD.Send (&sendHalo[srcHaloID][0], haloSize , MPI_DOUBLE, destCoreLinear, send_data_tag);
+			
 			
 		}
-		return ierr;
 	}
 
-	int haloRecv() {
-		int ierr;
+	void haloRecv() {
+		if (halo_width == 0) 
+			return;
 		for (int di = -1; di <= 1; di++) 
 		for (int dj = -1; dj <= 1; dj++)
 		for (int dk = -1; dk <= 1; dk++) 
@@ -418,7 +492,6 @@ public:
 				MPI::COMM_WORLD.Recv(&recvHalo[destHaloID][0], haloSize, MPI_DOUBLE, srcCoreLinear, send_data_tag);
 			}
 		}
-		return ierr;
 	}
 
 	void haloExchange() {
@@ -471,23 +544,27 @@ public:
 
 	void sendBackToRoot () {
 		MPI::COMM_WORLD.Send (&x_block[0], blockSize , MPI_DOUBLE, root_process, send_data_tag);
+		MPI::COMM_WORLD.Send (&this->normChange, 1 , MPI_DOUBLE, root_process, send_data_tag);
 	}
 
-	void syncBackAtRoot(vector<double> &x) {
+	double syncBackAtRoot(vector<double> &x) {
+		double normChange = 0.0;
 		for (int i_sub = 0; i_sub < iExtBlockSize; i_sub++) 
 		for (int j_sub = 0; j_sub < jExtBlockSize; j_sub++) 
 		for (int k_sub = 0; k_sub < kExtBlockSize; k_sub++) {
 			if (iStart + i_sub < 0 || iStart + i_sub >= i_bound || 
 				jStart + j_sub < 0 || jStart + j_sub >= j_bound || 
 				kStart + k_sub < 0 || kStart + k_sub >= k_bound ) 
-				continue;
+			continue;
 			int srcPos = 
 				  (iStart + i_sub) * j_bound * k_bound 
 				+ (jStart + j_sub) * k_bound 
 				+ (kStart + k_sub) ;
 			int targetPos = zmix(i_sub, j_sub, k_sub);
+			normChange +=  (x_block[targetPos] - x[srcPos]) * (x_block[targetPos] - x[srcPos]);
 			x[srcPos] = x_block[targetPos];
 		}
+		return normChange;
 	}
 
 	void betaSend() {
@@ -499,6 +576,23 @@ public:
 	}
 
 private:
+		// inner 
+	vector<vector<double > > haloInit () {
+		vector<vector<double > > vacantHalo(27);
+		for (int di = -1; di <= 1; di++) 
+		for (int dj = -1; dj <= 1; dj++)
+		for (int dk = -1; dk <= 1; dk++) 
+			if (di | dj | dk) { 
+				int haloLinearID = (1+di)*9 + (1+dj)*3 + (1+dk);
+				int haloWidth_i = di ? halo_width : iBlockSize;
+				int haloWidth_j = dj ? halo_width : jBlockSize;
+				int haloWidth_k = dk ? halo_width : kBlockSize;
+				int haloSize = haloWidth_i * haloWidth_j * haloWidth_k;
+				vacantHalo[haloLinearID] = vector<double> (haloSize, 0);
+			}
+		return vacantHalo;
+	}
+
 	inline void zNeighbors(vector<int> &zNeighborIndex, int centerIndex) {	    
 	    vector<int> i_comp, j_comp, k_comp;
 	    if (centerIndex & octMask_i)
@@ -525,12 +619,12 @@ private:
 	    	if ((i | j | k) != centerIndex)
 	        	zNeighborIndex.push_back(i | j | k);
 	    }
-	    
+	    	
 	}
 
 
 	inline int zmix (int i, int j, int k) {
-		// return seqMix(i, j, k);
+		return seqMix(i, j, k);
 		// int linearID = i * jExtBlockSize * kExtBlockSize + j * kExtBlockSize + k;
 		// if (memoZixL.count(linearID)) return memoZixL[linearID];
 	    
@@ -557,7 +651,7 @@ private:
 	}
 
 	inline vector<int> zDeMix(int zVal) {
-		// return seqDeMix(zVal); 
+		return seqDeMix(zVal); 
 		// if (memoDeZixL.count(zVal)) { 
 		// 	int seqVal = memoDeZixL[zVal];
 		// 	int k =  seqVal % kExtBlockSize;
@@ -588,54 +682,6 @@ private:
 		return {i, j, k};
 	}
 
-
-/*
-	inline int zmix (int i, int j, int k) {
-		// return seqMix(i, j, k);
-	    int zVal = 0;
-
-	    stringstream ss_i;
-		ss_i << (bitset<bitWidthDefault>) i;
-	    int i_comb;
-	    ss_i >> oct >> i_comb;
-
-	    stringstream ss_j;
-		ss_j << (bitset<bitWidthDefault>) j;
-	    int j_comb;
-	    ss_j >> oct >> j_comb;
-
-	    stringstream ss_k;
-		ss_k << (bitset<bitWidthDefault>) k;
-	    int k_comb;
-	    ss_k >> oct >> k_comb;
-
-
-	    
-		return (i_comb << 2) + (j_comb << 1) + k_comb;
-	}
-
-	inline vector<int> zDeMix (int zVal) {
-	    unsigned long int mask = 011111111111;
-	    bitset<bitWidthDefault> bs;
-
-	    stringstream ss_i;
-	    ss_i << oct << ((zVal>>2) & mask);
-	    ss_i >> oct >> bs;
-	    int i = bs.to_ulong();
-
-	    stringstream ss_j;
-	    ss_j << oct << ((zVal>>1) & mask);
-	    ss_j >> oct >> bs;
-	    int j = bs.to_ulong();
-
-	    stringstream ss_k;
-	    ss_k << oct << (zVal & mask);
-	    ss_k >> oct >> bs;
-	    int k = bs.to_ulong();
-
-		return {i, j, k};
-	}
-*/
 	inline 
 	int seqMix (int i, int j, int k) {
 		return i * jExtBlockSize * kExtBlockSize + j * kExtBlockSize + k;
@@ -695,21 +741,10 @@ int main (int argc, char *argv[]) {
 	dataSheet >> Capsule::i_bound >> Capsule::j_bound >> Capsule::k_bound;
 	int totalSize = Capsule::totalSize = Capsule::i_bound * Capsule::j_bound * Capsule::k_bound;
 
-	// Capsule mainCull (0, 0, 0);
-	// for (int i = 0; i < mainCull.iExtBlockSize; i++)
-	// for (int j = 0; j < mainCull.jExtBlockSize; j++)
-	// for (int k = 0; k < mainCull.kExtBlockSize; k++) {
-	// 	int zVal = mainCull.zmix(i, j, k);
-	// 	int linearID = i * mainCull.jExtBlockSize * mainCull.kExtBlockSize + 
-	// 				   j * mainCull.kExtBlockSize + k;
-	// 	memoZixL[linearID] = zVal;
-	// 	memoDeZixL[zVal] = linearID;
-	// }
-
 	memoInterFill(1024);
 
+	if (my_id == root_process) {
 
-	if (my_id == root_process) {		
 		vector<double> x (totalSize, 1.0);
 		vector<double> b (totalSize);
 		
@@ -720,6 +755,7 @@ int main (int argc, char *argv[]) {
 		for (double &entry : b) {
 			dataSheet >> entry;
 		}
+		double t0 = MPI::Wtime();
 		
 		for (int core_i = 0; core_i < numCore_i; core_i++)
 		for (int core_j = 0; core_j < numCore_j; core_j++)
@@ -735,14 +771,27 @@ int main (int argc, char *argv[]) {
 
 		}
 
+		double normChange = 0.0, max_elapsed_secs = 0.0;
 		for (int core_i = 0; core_i < numCore_i; core_i++)
 		for (int core_j = 0; core_j < numCore_j; core_j++)
 		for (int core_k = 0; core_k < numCore_k; core_k++) {
+			double err = 0.0, elapsed_secs = 0.0;
 			vector<int> core_pack = {core_i, core_j, core_k};
 			Capsule mainCull (core_i, core_j, core_k);
 			MPI::COMM_WORLD.Recv(&mainCull.x_block[0], mainCull.blockSize, MPI_DOUBLE, mainCull.core_linear, send_data_tag);
+			MPI::COMM_WORLD.Recv(&err, 1, MPI_DOUBLE, mainCull.core_linear, send_data_tag);
+			MPI::COMM_WORLD.Recv(&elapsed_secs, 1, MPI_DOUBLE, mainCull.core_linear, send_data_tag);
+			normChange += err; 
+			max_elapsed_secs = max (max_elapsed_secs, elapsed_secs);
 			mainCull.syncBackAtRoot(x);
 		}
+		normChange /= (num_procs_used - 1.0);
+
+		double t1 = MPI::Wtime();
+		double elapsed_secs = t1 - t0;
+		
+		// printf("Core::%02i, seconds_elapsed : %f:: Error: %f, Steps : %i (%f, %f) \n", my_id, elapsed_secs, normChange, iterationSteps, t0, t1);
+		printf("%f\t%f\t%d\n", max_elapsed_secs, normChange, iterationSteps);
 		/*
 		for (double val : x) { 
 			printf("%f\n", val);
@@ -757,24 +806,27 @@ int main (int argc, char *argv[]) {
 		MPI::COMM_WORLD.Recv (&capsule.x_block[0], capsule.blockSize, MPI_DOUBLE, root_process, send_data_tag);
 		MPI::COMM_WORLD.Recv (&capsule.b_block[0], capsule.blockSize, MPI_DOUBLE, root_process, send_data_tag);
 		
-		int n = iterationSteps;
-		double err = 1, max_err = 1;
-		clock_t t0 = clock();
-		while (n--) {
-			err = capsule.GS_Z_block_update003();
-			// err = capsule.GS_Z_block_update(); 
-			capsule.prepareSendHalo();
-			capsule.haloExchange(); // capsule.haloSend();  capsule.haloRecv();
-			capsule.updateFromRecvHalo();
-			
-			// MPI::COMM_WORLD.Reduce(&err, &max_err, 1, MPI::DOUBLE, MPI::MAX, 0);
-			// MPI::COMM_WORLD.Bcast (&max_err, 1, MPI_INT, 0);
-		}
+
 		
-		clock_t t1 = clock();
-    	double elapsed_secs = double(t1 - t0) / CLOCKS_PER_SEC;
-		printf("Core: %02i, seconds_elapsed : %f:: Error: %f, Steps : %i \n", my_id, elapsed_secs, err, iterationSteps);
+		double err = 1, max_err = 1;
+		double t0 = MPI::Wtime();
+		for (int i = 0; i < iterationSteps; i++) {
+			err = capsule.GS_Z_block_update004(); 
+			// err = capsule.GS_Z_block_update(); 
+			if (i % 8 == (capsule.halo_width - 1))  {
+				capsule.prepareSendHalo();
+				capsule.haloExchange(); // capsule.haloSend();  capsule.haloRecv();
+				capsule.updateFromRecvHalo();
+				capsule.halo_noneUpdate_width %= capsule.halo_width; 
+				if (capsule.halo_noneUpdate_width == 0) 
+					capsule.halo_noneUpdate_width = capsule.halo_width; 
+			}
+		}
+		double t1 = MPI::Wtime();
+    	double elapsed_secs = t1 - t0;
+		// printf("Core: %02i, seconds_elapsed : %f:: Error: %f, Steps : %i (%f, %f) \n", my_id, elapsed_secs, err, iterationSteps, t0, t1);
 		capsule.sendBackToRoot();
+		MPI::COMM_WORLD.Send (&elapsed_secs, 1 , MPI_DOUBLE, root_process, send_data_tag);
 		
 	}
 	MPI::Finalize();
